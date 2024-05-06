381204729789a3527c85421719e8133eba0779bc
Mihalis <mukhortovm2004@mail.ru>
2024-05-06 23:56:43 +0500

    Add T5 normalization of texts

--- services/whisper_service.py
+++ services/whisper_service.py
@@ -7,6 +7,8 @@ import torch
 
 import whisper_s2t
 from dto.transcribation_request import TranscribationRequest
+from services.diarizer_service import DiarizerService
+from services.t5_service import T5Service
 from utils.helpers import *
 from whisper_s2t.backends.ctranslate2.model import WhisperModelCT2
 
@@ -17,7 +19,7 @@ class WhisperService:
 
         self.__arch: str = "large-v2"  # large-v3 produces MORE hallucinations
         self.__device: str = "cuda"
-        self.__batch_size: int = 128
+        self.__batch_size: int = 64
         self.__compute_type: str = "bfloat16"
         self.__language: str = "ru"
         self.__asr_options: dict = {
@@ -43,6 +45,12 @@ class WhisperService:
             compute_type=self.__compute_type,
             asr_options=self.__asr_options
         )
+
+        self.__logger.debug("DiarizerService initialization...")
+        self.__diarizer_service = DiarizerService()
+
+        self.__logger.debug("T5Service initialization...")
+        self.__t5_service = T5Service()
 
         self.__logger.debug(f"WhisperService initialized!")
 
@@ -80,36 +88,38 @@ class WhisperService:
         self.__logger.debug("Concatenating audios...")
         await concat_audio([id_t_request[1].temp_dir for id_t_request in self.__tasks])
 
-        self.__logger.debug("Creating the nemo diarization process...")
-        nemo_process = await asyncio.create_subprocess_exec(*[
-            "python", self.__nemo_process_py, self.__device
-        ], stdout=DEVNULL, stderr=DEVNULL)
-
-        self.__logger.debug("Creating transcribation...")
+        self.__logger.debug("Creating the transcribation...")
         transcribed_results: list[list[dict]] = self.__whisper.transcribe_with_vad(
             [id_t_request[1].audio_file for id_t_request in self.__tasks],
             batch_size=self.__batch_size, lang_codes=[self.__language], tasks=["transcribe"]
         )
 
-        self.__logger.debug("Waiting for the nemo diarization process finish...")
-        await nemo_process.wait()
-        speaker_tss: list[list[list[int]]] = split_nemo_result(
-            await read_nemo_result("/tmp/asrme_concatenated"),
+        self.__logger.debug("Creating the diarization...")
+        speaker_tss: list[list[list[int]]] = await self.__diarizer_service.diarize(
             [int(id_t_request[1].audio_length * 1000) for id_t_request in self.__tasks]
         )
 
         self.__logger.debug("Calculating the results...")
         for i, result in enumerate(transcribed_results):
             os.system(f"rm -rf {self.__tasks[i][1].temp_dir}/")
-            self.__results[self.__tasks[i][0]] = detect_admin_and_patient(
-                assign_diarization_to_transcribation(
-                    speaker_tss[i],
-                    list(chain.from_iterable(seg["word_timestamps"] for seg in result))
-                )
+
+            assigned_speech: list[list[str]] = assign_diarization_to_transcribation(
+                speaker_tss[i],
+                list(chain.from_iterable(seg["word_timestamps"] for seg in result))
             )
+            for j, phrase in enumerate(
+                    self.__t5_service.normalize([speaker_phrase[1] for speaker_phrase in assigned_speech])
+            ):
+                assigned_speech[j][1] = phrase
+
+            self.__results[self.__tasks[i][0]] = detect_admin_and_patient(assigned_speech)
+
+            self.__logger.debug(f"Calculated {i + 1}/{len(transcribed_results)} result")
 
     def dispose(self):
         del self.__whisper
+        del self.__diarizer_service
+        del self.__t5_service
         torch.cuda.empty_cache()
 
         os.system(f"rm -rf /tmp/asrme_*")
